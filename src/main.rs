37d539bae50c9249a007a68fe8dd9b070dff4a98
sy3c4ll <sy3c4ll@gmail.com>
2025-05-31 01:57:40 +0900

    Output as PBM P1

--- src/main.rs
+++ src/main.rs
@@ -15,12 +15,14 @@ impl<const W: usize, const H: usize> Default for Image<W, H> {
 
 impl<const W: usize, const H: usize> fmt::Display for Image<W, H> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_str("P1\n")?;
+        writeln!(f, "{W} {H}")?;
         for y in 0..H {
             for x in 0..W {
-                f.write_str(if self.pixels[x][y] { "x" } else { "." })?;
+                f.write_str(if self.pixels[x][y] { "1" } else { "0" })?;
             }
-            f.write_str("\n")?;
         }
+        f.write_str("\n")?;
         Ok(())
     }
 }
@@ -32,7 +34,8 @@ fn predicate_circle(centre: (usize, usize), radius: usize) -> impl Fn((usize, us
 fn main() {
     const W: usize = 64;
     const H: usize = 48;
-    let predicate = predicate_circle((32, 24), 5);
+    const R: usize = 5;
+    let predicate = predicate_circle((W / 2, H / 2), R);
     let mut image = Image::<W, H>::default();
     for y in 0..H {
         for x in 0..W {
